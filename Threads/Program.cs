21c87c9a88a031b09ea9602f153f8341e55ba534
ana.predescu <ana.predescu@teamnet.ro>
2016-06-30 14:16:50 +0300

    added Threads project

---
+++ Threads/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        private static void Main()
+        {
+            //Threads
+            //ThreadTest.Execute();
+            //SharringDataTest.Execute();
+            //LockerObjectTest.Execute();
+            //JoinSleepTest.Execute();
+            //ThreadPoolTest.Execute();
+
+            //Tasks
+            TasksTest.Execute();
+        }
+    }
+
+}
